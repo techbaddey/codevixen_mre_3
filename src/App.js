9cbe0589c218b8abc4525584184d4535d1019477
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2023-06-09 22:50:46 +0100

    Updated my files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 import useApiRequest from './useApiRequest';
 
