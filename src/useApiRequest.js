ed823f189dcf92dc3091d581748a3d8741953f95
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2023-06-09 22:34:16 +0100

    pushing all my exam files

---
+++ src/useApiRequest.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import axios from 'axios';
+
+const useApiRequest = url => {
+  const [data, setData] = React.useState([]);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, error, loading };
+};
+
+export default useApiRequest;
